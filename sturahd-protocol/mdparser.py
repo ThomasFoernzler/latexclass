48fb60223cf14b47f9acf3c3910bd6e2b674984b
Thomas Förnzler <thomas.foernzler@mailbox.org>
2020-12-19 01:25:14 +0100

    added a markdown parser to convert a raw discussion txt file to latex code

---
+++ sturahd-protocol/mdparser.py
@@ -0,0 +1,48 @@
+sitzung = input("Sitzungsnummer angeben: ")
+
+with open(sitzung + "-protokoll" + "/" + sitzung + "-raw.txt", "r") as f:
+    raw=f.readlines()
+out = open(sitzung + "-protokoll" + "/" + sitzung + "-discussion.tex", "w")
+
+#print(raw)
+
+firsttime = True
+alsofirsttime = True
+
+for num, line in enumerate(raw, start=1 ):
+    if line == "\n":
+        continue
+    if line[0:2] == "# ":
+        continue
+    if line[:3] == "## ":
+        if firsttime == False:
+            out.write("}")
+        out.write("\n" + "\n")
+        firsttime = False
+        out.write(line[3:])
+        diretlyfollowing = True
+        continue
+    if line[0:4] == "### ":
+        if diretlyfollowing == True:
+            out.write("\n")
+            out.write(line[4:])
+            out.write(r"\textbf{X. Lesung}"+"\n"+r"\ul{"+ "\n")
+            diretlyfollowing = False
+        if diretlyfollowing == False:
+            out.write("}\n\n")       
+            out.write(line[4:])
+            out.write(r"\textbf{X. Lesung}"+"\n"+r"\ul{"+ "\n")
+        continue
+    if line[0:2] == "- ":
+        out.write("\t" + r"\li{" + line[2:-1] + "}\n")
+        continue
+    if line[0:5] == "   - ":
+        out.write("\t\t" + r"\noli{" + r"\ul{" + "\n" + "\t\t"+ r"\lii{"+ line[5:-1] + "\n" + "\t\t" + r"}}}" + "\n")
+        continue
+    else:
+        # out.write(line)
+        print("weird behavior in line " , num)
+out.write("}")
+
+out.close()
+print("finished")
